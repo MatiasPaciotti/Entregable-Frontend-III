c761a4fb5822f810527b799f11ccdf516ee447bb
Matias Paciotti <mpaciottiiacchelli@gmail.com>
2022-08-31 23:43:43 -0300

    Archivo entregable Frontend III

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
 
-// El componente Item no tiene componentes hijos.
-// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
-// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
-// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
-// Maqueta de Item:
-//    h3
-//    p
-//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
-//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+export default function Item( {product, setCounter} ) {
 
-export default function Item() {
+  const { producto, stock } = product;
+  const { nombre, descripcion } = producto;
+  
+  const [total, setTotal] = useState(stock)
+
+  const handleClick = () => {
+      setTotal(t => t - 1);
+      setCounter(c => c + 1);
+  }
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3>{ nombre }</h3>
+      <p>{ descripcion }</p>
+      <h5>En stock:  
+        <span className={ total === 0 ? 'agotado' : '' }>
+            { total === 0 ? 'AGOTADO' : total }
+        </span>
+      </h5>
+      <button
+        onClick={ handleClick }
+        disabled={ total === 0 }
+      >
+        { total > 0  ? 'COMPRAR' : 'SIN STOCK' }
+      </button>
     </div>
   )
 }
